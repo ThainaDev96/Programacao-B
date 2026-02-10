63dd65acbbce669d1a41c6a91280b42970eaad42
Thaina Ribeiro <rthaina776@gmail.com>
2026-02-10 20:31:56 -0300

    Update LerArquivo.java

--- LeituraArquivo/LerArquivo.java
+++ LeituraArquivo/LerArquivo.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 /**
  *
- * @author ThainaRibeiroBairros
+ * @author ThainaRibeiro
  */
 public class LerArquivo {
 
@@ -39,3 +39,4 @@ public class LerArquivo {
     }   
     
 }
+
